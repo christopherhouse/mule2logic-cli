a950d3caab3ad683cb3bb273fe37703968bca1ce
Christopher House <chhouse@microsoft.com>
2026-04-09 14:24:37 -0500

    initial repo structure

---
+++ src/cli.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import { convertCommand } from './commands/convert.js';
+
+program
+  .name('mule2logic')
+  .description('Convert MuleSoft XML flows to Azure Logic Apps Standard workflow JSON')
+  .version('1.0.0');
+
+program
+  .command('convert <input>')
+  .description('Convert a MuleSoft XML flow to Logic Apps JSON')
+  .option('--output <file>', 'Write JSON to file')
+  .option('--explain', 'Include explanation')
+  .option('--pretty', 'Pretty-print JSON')
+  .option('--verbose', 'Debug logs')
+  .action(convertCommand);
+
+program.parse();
